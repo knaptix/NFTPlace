0849051cfed8ecfc53fc185ad782c714be6c6a14
karantec <karanrana3095@gmail.com>
2025-01-03 13:20:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import React, { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./Component/Dashboard/DashboardLayout";
-import Login from "./Component/Login";
-
-
-// A Private Route component to protect routes
-const PrivateRoute = ({ isAuthenticated, children }) => {
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
+import Home from "./Component/Home"
+import Profile from "./Component/Profile"
 
 const App = () => {
-  // Simulating authentication state (replace this with your actual auth logic)
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
-
   return (
-    <Routes>
-      {/* Public Login Page */}
-      <Route path="/login" element={<Login />} />
+    <div>
+      <Home/>
+      <Profile/>
+      
+    </div>
+  )
+}
 
-      {/* Protected Dashboard */}
-      <Route
-        path="/dashboard/*"
-        element={
-          <PrivateRoute isAuthenticated={isAuthenticated}>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
-
-      {/* Default Route */}
-      <Route
-        path="/"
-        element={<Navigate to={isAuthenticated ? "/dashboard/home" : "/login"} />}
-      />
-    </Routes>
-  );
-};
-
-export default App;
+export default App
