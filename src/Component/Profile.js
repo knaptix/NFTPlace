0e1d986e20731e1d4bcf77f5afda96d92df7a246
Adarsh Prakash <adarshnobilian@gmail.com>
2025-01-06 17:35:53 +0530

    testimonials added, help, profile,home components deleted

--- src/Component/Profile.js
+++
@@ -1,8 +0,0 @@
-const Profile = () => (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Profile Component</h1>
-      <p>Welcome to the dashboard home!</p>
-    </div>
-  );
-
-export default Profile;
