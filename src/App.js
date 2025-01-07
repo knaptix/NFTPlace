b040e28fb0b1e39e8d677a297760586e194dbdbc
RenderHub01 <renderhub01@gmail.com>
2025-01-07 12:18:24 +0000

    changesdone

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Navbar from "./Component/Navbar"
-import HeroSection from "./Component/HeroSection"
-import ProductsSection from "./Component/ProductsSection"
+import Navbar from "./Component/Navbar";
+import HeroSection from "./Component/HeroSection";
+import ProductsSection from "./Component/ProductsSection";
 import './styles/swiper-custom.css';
 import Testimonials from "./Component/Testimonials";
 import Footer from "./Component/Footer";
@@ -9,42 +9,32 @@ import CartSidebar from "./Component/CartSidebar";
 import OrderCheckout from "./Component/OrderCheckout";
 import React, { useState } from "react";
 
-
 const App = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [showCheckout, setShowCheckout] = useState(false);
-  const handleCartClick = () => {
-    setIsCartOpen(true);
-  };
 
-  const handleCartClose = () => {
-    setIsCartOpen(false);
-  };
-
-  const handleCheckout = () => {
-    setShowCheckout(true);
-    setIsCartOpen(false); // Close cart when proceeding to checkout
-  };
   return (
     <div>
-      <Navbar onCartClick={() => setIsCartOpen(true)}/>
+      <Navbar onCartClick={() => setIsCartOpen(true)} />
       <CartSidebar 
         isOpen={isCartOpen}
         onClose={() => setIsCartOpen(false)}
-        onCheckout={() => setShowCheckout(true)}
+        onCheckout={() => {
+          setShowCheckout(true);
+          setIsCartOpen(false); // Close cart when proceeding to checkout
+        }}
       />
-
       <OrderCheckout 
         isVisible={showCheckout}
         onClose={() => setShowCheckout(false)}
       />
       <HeroSection />
       <ProductsSection />
-      <BulkProductSearch/>
-      <Testimonials/>
-      <Footer/>
+      <BulkProductSearch />
+      <Testimonials />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
